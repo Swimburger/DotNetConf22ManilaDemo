77bc99c384031db01bf403bccd2167ec9bc99a4c
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-01-20 23:21:04 -0500

    Add project files.

---
+++ ControllerSample.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace TwilioWebhookDemo
+{
+    public class ControllerSample : TwilioController
+    {
+        [HttpPost("/message")]
+        public IActionResult Message(SmsRequest smsRequest)
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message($"You said: {smsRequest.Body}");
+            return TwiML(messagingResponse);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.HttpLogging;
+using System.Text;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+var devTunnelUrl = Environment.GetEnvironmentVariable("VS_TUNNEL_URL");
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region HTTP Logging
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.MediaTypeOptions.AddText("application/x-www-form-urlencoded", Encoding.UTF8);
+    options.RequestHeaders.Add("x-twilio-signature");
+});
+#endregion
+
+#region Request Validation
+//builder.Services.AddTwilioRequestValidation((provider, options) =>
+//{
+//    builder.Configuration.GetSection("Twilio:RequestValidation").Bind(options);
+//    if (devTunnelUrl != null)
+//        options.BaseUrlOverride = devTunnelUrl;
+//});
+#endregion
+
+var app = builder.Build();
+
+#region HTTP Logging
+app.UseHttpLogging();
+
+// force body to be read for the sake of HTTP logging middleware
+// the logging middleware only logs the body if it is read by the app.
+app.Use(async (context, next) =>
+{
+    context.Request.EnableBuffering();
+    await new StreamReader(context.Request.Body)
+        .ReadToEndAsync(context.RequestAborted)
+        .ConfigureAwait(false);
+    context.Request.Body.Position = 0;
+    await next();
+});
+#endregion
+
+app.MapPost("/message", () => new MessagingResponse()
+        .Message("Ahoy .NET Conf, Manila!")
+        .ToTwiMLResult()
+);
+
+//app.MapPost("/message", async (HttpRequest request, CancellationToken cancellationToken) =>
+//{
+//    var form = await request.ReadFormAsync(cancellationToken).ConfigureAwait(false);
+//    var body = form["Body"];
+
+//    return new MessagingResponse()
+//        .Message($"You said: {body}")
+//        .ToTwiMLResult();
+//});
+
+app.Run();
